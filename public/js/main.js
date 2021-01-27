1ec8bbc06edfa1e94bc6da0a43841b176cc883e7
Achraf <hardy1348@live.fr>
2021-01-27 13:40:04 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+// Exo1
+
+// let contenu = document.getElementsByTagName('h1')
+// console.log(contenu);
+
+// Exo2
+
+// let contenu1 = document.getElementsByTagName('h3')
+// console.log(contenu1);
+// let prems = contenu1[0]
+// let prems1 = contenu1[1]
+// let prems2 = contenu1[2]
+
+// console.log(prems);
+// console.log(prems1);
+// console.log(prems2);
+
+// //3
+// let para = document.getElementsByTagName('p')
+// console.log(para);
+// let secondPara = para[1]
+// console.log(secondPara);
+
+// //4
+// let allLi = document.getElementsByTagName('li')
+// console.log(allLi);
+
+// let threeLi = allLi[2]
+// console.log(threeLi);
